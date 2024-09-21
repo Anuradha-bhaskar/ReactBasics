69843c7383ea812fa68dc41657c839f2e11f2b1d
Anuradha Bhaskar <anuradha.t.bhaskar@gmail.com>
2024-09-21 15:37:20 +0530

    first commit

---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,17 @@
+import { CORE_CONCEPTS } from "../data.js";
+import CoreConcept from "./CoreConcept.jsx";
+import Section from "./Section.jsx";
+
+export default function CoreConcepts(){
+    return (
+        <Section id="core-concepts" title = "Core Concepts">
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key ={conceptItem.title} {...conceptItem}/> )}
+            {/* <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/> */}
+          </ul>
+        </Section>
+    );
+}
